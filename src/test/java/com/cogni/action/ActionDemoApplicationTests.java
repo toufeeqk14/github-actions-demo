9bb5c501b9f3a93676fd150c5d32d71b737e4058
Khan <662501@cognizant.com>
2023-02-21 20:51:22 +0530

    adding a simple test case

--- src/test/java/com/cogni/action/ActionDemoApplicationTests.java
+++ src/test/java/com/cogni/action/ActionDemoApplicationTests.java
@@ -1,5 +1,7 @@
 package com.cogni.action;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,6 +12,10 @@ class ActionDemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	@Test
+	void test() {
+		assertEquals("khan", "khan");
 	}
 
 }
