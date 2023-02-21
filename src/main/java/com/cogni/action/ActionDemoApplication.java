5354df6284867aaa5057f4a6e26d1b95eaec940c
Khan <662501@cognizant.com>
2023-02-21 14:47:57 +0530

    initial commit

---
+++ src/main/java/com/cogni/action/ActionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cogni.action;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionDemoApplication.class, args);
+	}
+
+}
